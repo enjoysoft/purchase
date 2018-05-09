b018d6b58afa01255d7fbbe9626965b6f60e2e65
Jerry Junhao Ying <enjoysoft@hotmail.com>
2018-05-09 22:51:37 +0800

    fixes to return only detail. word generation.

--- data.py
+++ data.py
@@ -1,19 +1,49 @@
 import pandas as pd
-
+import docx
 
 vendor_label: str = '供应商'
 quote_label: str = '报价'
 index_label: str = '序号'
 formatting: str = '%.2f'
 unit_label: str = '单价'
+no_quote: str = '无法报价'
 
-vendors: list = ['科罕', '中航供销', '美隆航', '韦斯科', '屹领', '赛方', '润洽']
+df: pd.DataFrame = pd.read_excel('bay.xlsx', sheet_name=0, na_values=no_quote, index_col=0)
 
-df: pd.DataFrame = pd.read_clipboard(sep='\t', na_values='无法报价')
-df = df.set_index(index_label)
 
+# prepare word document
+def export(name: str, df: pd.DataFrame, quote_detail: list) -> None:
+    doc = docx.Document()
+    # add a table to the end and create a reference variable
+    # extra row is so we can add the header row
+    t = doc.add_table(df.shape[0] + 1, df.shape[1] + 1)
+    t.style = 'TableGrid'
+
+    t.cell(0, 0).text = index_label
+    # add the header rows.
+    for j in range(df.shape[-1]):
+        t.cell(0, j + 1).text = df.columns[j]
+
+    # add the header columns.
+    for i in range(df.shape[0]):
+        t.cell(i + 1, 0).text = str(df.index[i])
+
+    # add the rest of the data frame
+    for i in range(df.shape[0]):
+        for j in range(df.shape[-1]):
+            t.cell(i + 1, j + 1).text = str(df.values[i, j])
+
+    for index, many_df in quote_detail:
+        doc.add_paragraph(index_label + ','.join(map(str, index)))
+        doc.add_paragraph(many_df.to_csv(sep='\t', float_format=formatting))
+
+    # save the doc
+    doc.save('./' + name + '.docx')
+    return None
+
+
+vendors = pd.read_excel('bay.xlsx', sheet_name=1, header=None).iloc[:, 0].tolist()
 quotes = df.loc[:, vendors]
-
 
 selection = quotes.idxmin(1)
 selection.name = vendor_label
@@ -26,36 +56,30 @@ df = df.join(selection)
 df = df.join(selection_quotes)
 df = df.join(unit_quotes)
 
-
 groups = [group for group in df.groupby(vendor_label)]
+
+no_quotes = df.loc[df[vendor_label].isna()].drop(vendors, 1).drop(vendor_label, 1)
+print('无报价')
+print(no_quotes.to_csv(sep='\t', float_format=formatting))
 
 for vendor_name, vendor_df in groups:
     print('=============================')
     print(vendor_name)
     print('=============================')
 
-    vendor_product = vendor_df.drop(vendors, 1)
+    vendor_product = vendor_df.drop(vendors, 1).drop(vendor_label, 1)
     print('总价', formatting % vendor_df[quote_label].sum())
     print(vendor_product.to_csv(sep='\t', float_format=formatting))
 
     vendor_quotes = vendor_df.loc[:, vendors]
-    quote_count = vendor_quotes.count(1)
-    (many, few) = (vendor_quotes.loc[quote_count >= 3], vendor_quotes.loc[quote_count < 3])
 
-    few_detail = [(row[0], row[1].dropna().sort_values()) for row in few.iterrows()]
+    quote_groups = vendor_quotes.notna().groupby(vendors).groups
+    quote_detail = [(v.tolist(), vendor_quotes.loc[v].sum(skipna=False).sort_values().fillna(no_quote)) for k, v in
+                    quote_groups.items()]
 
-    if not few.empty:
-        print(">>> 过少报价")
-    for index, few_df in few_detail:
-        print("序号:", index)
-        print(few_df.to_csv(sep='\t', float_format=formatting))
-
-    many_notna = many.notna()
-    many_groups = many_notna.groupby(vendors).groups
-    many_detail = [(v.tolist(), many.loc[v].sum(skipna=False).dropna().sort_values()) for k, v in many_groups.items()]
-
-    if not many.empty:
-        print('>>> 其它报价')
-    for index, many_df in many_detail:
+    print('>>> 明细')
+    for index, many_df in quote_detail:
         print("序号:", ','.join(map(str, index)))
         print(many_df.to_csv(sep='\t', float_format=formatting))
+
+    export(vendor_name, vendor_product, quote_detail)
