827ca5882fbcec109ae25ff40c90bedd643c9f19
Jerry Junhao Ying <enjoysoft@hotmail.com>
2018-05-14 23:34:02 +0800

    add template word and excel.

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
+import io
+
 import pandas as pd
-import docx
 
 vendor_label: str = '供应商'
 quote_label: str = '报价'
@@ -7,42 +8,15 @@ index_label: str = '序号'
 formatting: str = '%.2f'
 unit_label: str = '单价'
 no_quote: str = '无法报价'
+excel_filename: str = 'bay.xlsx'
 
-df: pd.DataFrame = pd.read_excel('bay.xlsx', sheet_name=0, na_values=no_quote, index_col=0)
+df: pd.DataFrame = pd.read_excel(excel_filename, sheet_name=0, na_values=no_quote, index_col=0)
 
+vendors_raw: pd.Series = pd.read_excel(excel_filename, sheet_name=1, header=None, index_col=0, squeeze=True)
+vendors_name = vendors_raw.index
+vendors = list(filter(lambda v: v in df, vendors_raw.index))
+vendors_timeout = list(filter(lambda v: v not in df.columns, vendors_raw.index))
 
-# prepare word document
-def export(name: str, df: pd.DataFrame, quote_detail: list) -> None:
-    doc = docx.Document()
-    # add a table to the end and create a reference variable
-    # extra row is so we can add the header row
-    t = doc.add_table(df.shape[0] + 1, df.shape[1] + 1)
-    t.style = 'TableGrid'
-
-    t.cell(0, 0).text = index_label
-    # add the header rows.
-    for j in range(df.shape[-1]):
-        t.cell(0, j + 1).text = df.columns[j]
-
-    # add the header columns.
-    for i in range(df.shape[0]):
-        t.cell(i + 1, 0).text = str(df.index[i])
-
-    # add the rest of the data frame
-    for i in range(df.shape[0]):
-        for j in range(df.shape[-1]):
-            t.cell(i + 1, j + 1).text = str(df.values[i, j])
-
-    for index, many_df in quote_detail:
-        doc.add_paragraph(index_label + ','.join(map(str, index)))
-        doc.add_paragraph(many_df.to_csv(sep='\t', float_format=formatting))
-
-    # save the doc
-    doc.save('./' + name + '.docx')
-    return None
-
-
-vendors = pd.read_excel('bay.xlsx', sheet_name=1, header=None).iloc[:, 0].tolist()
 quotes = df.loc[:, vendors]
 
 selection = quotes.idxmin(1)
@@ -58,6 +32,8 @@ df = df.join(unit_quotes)
 
 groups = [group for group in df.groupby(vendor_label)]
 
+excel_writer = pd.ExcelWriter('final.xlsx')
+
 no_quotes = df.loc[df[vendor_label].isna()].drop(vendors, 1).drop(vendor_label, 1)
 print('无报价')
 print(no_quotes.to_csv(sep='\t', float_format=formatting))
@@ -71,15 +47,32 @@ for vendor_name, vendor_df in groups:
     print('总价', formatting % vendor_df[quote_label].sum())
     print(vendor_product.to_csv(sep='\t', float_format=formatting))
 
+    numbers = set(vendor_product['编号'].unique())
+    print(numbers)
+
     vendor_quotes = vendor_df.loc[:, vendors]
 
     quote_groups = vendor_quotes.notna().groupby(vendors).groups
-    quote_detail = [(v.tolist(), vendor_quotes.loc[v].sum(skipna=False).sort_values().fillna(no_quote)) for k, v in
+    quote_detail = [(v.tolist(), vendor_quotes.loc[v].sum(skipna=False).sort_values()) for k, v in
                     quote_groups.items()]
 
-    print('>>> 明细')
-    for index, many_df in quote_detail:
-        print("序号:", ','.join(map(str, index)))
-        print(many_df.to_csv(sep='\t', float_format=formatting))
+    detail = io.StringIO()
 
-    export(vendor_name, vendor_product, quote_detail)
+    for index, detail_df in quote_detail:
+        print("询价单", ','.join(map(str, index)), '项', file=detail)
+        for detail_vendor, row in detail_df.items():
+            if pd.isna(row):
+                print(vendors_raw[detail_vendor], '回复无法报价', file=detail)
+            else:
+                print(vendors_raw[detail_vendor], '报价人民币', '%.2f' % row, '元', file=detail)
+        for v in vendors_timeout:
+            print(vendors_raw[v], '逾期未回复', file=detail)
+        print(file=detail)
+    detail_str: str = detail.getvalue()
+    detail.close()
+
+    print(detail_str)
+
+    vendor_product.to_excel(excel_writer, sheet_name=vendor_name)
+
+excel_writer.save()
