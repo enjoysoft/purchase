c9a13e344de62d40c3c0ee0d3571977f8ff273ac
Jerry Junhao Ying <enjoysoft@hotmail.com>
2018-05-16 00:21:11 +0800

    use template

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import io
 
+import docxtpl
 import pandas as pd
+from docxtpl import DocxTemplate
 
 vendor_label: str = '供应商'
 quote_label: str = '报价'
@@ -34,6 +36,9 @@ groups = [group for group in df.groupby(vendor_label)]
 
 excel_writer = pd.ExcelWriter('final.xlsx')
 
+vendor_all = len(vendors_raw)
+vendor_count = len(quotes.columns[~quotes.isna().all()])
+
 no_quotes = df.loc[df[vendor_label].isna()].drop(vendors, 1).drop(vendor_label, 1)
 print('无报价')
 print(no_quotes.to_csv(sep='\t', float_format=formatting))
@@ -44,10 +49,11 @@ for vendor_name, vendor_df in groups:
     print('=============================')
 
     vendor_product = vendor_df.drop(vendors, 1).drop(vendor_label, 1)
-    print('总价', formatting % vendor_df[quote_label].sum())
+    total: str = formatting % vendor_df[quote_label].sum()
+    print('总价', total)
     print(vendor_product.to_csv(sep='\t', float_format=formatting))
 
-    numbers = set(vendor_product['编号'].unique())
+    numbers = ','.join(map(str, vendor_product['编号'].unique()))
     print(numbers)
 
     vendor_quotes = vendor_df.loc[:, vendors]
@@ -56,10 +62,12 @@ for vendor_name, vendor_df in groups:
     quote_detail = [(v.tolist(), vendor_quotes.loc[v].sum(skipna=False).sort_values()) for k, v in
                     quote_groups.items()]
 
-    detail = io.StringIO()
+    detail: io.StringIO = io.StringIO()
+    vendor_detail: io.StringIO = io.StringIO()
 
     for index, detail_df in quote_detail:
-        print("询价单", ','.join(map(str, index)), '项', file=detail)
+        print("询价单第", ','.join(map(str, index)), '项', file=detail)
+        print("询价单第", ','.join(map(str, index)), '项共有', detail_df.count(), '家供应商', file=vendor_detail)
         for detail_vendor, row in detail_df.items():
             if pd.isna(row):
                 print(vendors_raw[detail_vendor], '回复无法报价', file=detail)
@@ -68,10 +76,27 @@ for vendor_name, vendor_df in groups:
         for v in vendors_timeout:
             print(vendors_raw[v], '逾期未回复', file=detail)
         print(file=detail)
+
     detail_str: str = detail.getvalue()
+    vendor_detail_str: str = vendor_detail.getvalue()
     detail.close()
+    vendor_detail.close()
 
     print(detail_str)
+
+    doc = DocxTemplate("template.docx")
+
+    context = {'numbers': numbers,
+               'vendor': vendors_raw[vendor_name],
+               'detail': docxtpl.R(detail_str),
+               'total': total,
+               'vendor_all': vendor_all,
+               'vendor_count': vendor_count,
+               'vendor_detail': docxtpl.R(vendor_detail_str)
+               }
+    print(context)
+    doc.render(context)
+    doc.save(vendor_name + ".docx")
 
     vendor_product.to_excel(excel_writer, sheet_name=vendor_name)
 
